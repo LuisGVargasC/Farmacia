a63ba6daaedeff0fe0be205c32bc44cf1b115b22
Luis Gabriel Vargas Cano <16470420@itcampeche.edu.mx>
2019-12-05 04:47:48 -0600

    Farmacia 1.2 Conexion

---
+++ conexion/Connection.php
@@ -0,0 +1,16 @@
+
+<?php
+$serverName = "DESKTOP-Q1QEVNS\ "; //serverName\instanceName
+
+// Puesto que no se han especificado UID ni PWD en el array  $connectionInfo,
+// La conexión se intentará utilizando la autenticación Windows.
+$connectionInfo = array( "Database"=>"dbName");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
